2dbe1003af1fa904845ddea0e64f2de479f644cb
jacobhjustice <jacobhj1997@gmail.com>
2018-12-25 16:58:47 -0600

    Server function stubs/setup for db access

---
+++ server_functions.php
@@ -0,0 +1,25 @@
+<?php
+    function createGame($conn, $creatorID, $challengedID) {
+
+    }
+
+    function getLatestMove($conn, $gameID, $turnNum) {
+
+    }
+
+    function createUser($conn, $username, $password) {
+
+    }
+
+    function takeTurn($conn, $turn, $cellID, $squareID, $nextSquareID, $playerID, $tookSquare, $wonGame) {
+
+    }
+
+    function allGames($conn, $userID) {
+
+    }
+
+    function checkForTurn($conn, $gameID) {
+
+    }
+?>
---
+++ server_requests.php
@@ -0,0 +1,9 @@
+<?php
+    // All variable used in database access are stored in secret.php are listed below:
+    //  $server: The server on which the database exists
+    //  $user: the username credentials to access the database
+    //  $password: Password associated with username
+    //  $db: The name of the database within the server
+    require 'secret.php';
+    require 'server_functions.php';
+?>
